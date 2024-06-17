4ea72c6a91ad992b4a9c594509cfb9dc767d7309
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-06-17 10:03:27 +0200

    Initial commit

---
+++ src/main/java/CarloPatalano/u5_d1/U5D1Application.java
@@ -0,0 +1,12 @@
+package CarloPatalano.u5_d1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5D1Application {
+	public static void main(String[] args) {
+		SpringApplication.run(U5D1Application.class, args);
+	}
+
+}
---
+++ src/test/java/CarloPatalano/u5_d1/U5D1ApplicationTests.java
@@ -0,0 +1,13 @@
+package CarloPatalano.u5_d1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5D1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
